9e508cd71bac4832157c1dbcd9cdbcef04a6c9de
3ll3d00d <mattkhan@gmail.com>
2018-08-18 21:52:07 +0200

    Converted to internal biquad filter calculator including ability to create multi biquad filters

--- src/test/python/main.py
+++ src/test/python/main.py
@@ -1,20 +1,17 @@
+import matplotlib.pyplot as plt
 import numpy as np
-from matplotlib import pyplot as mp
 from scipy import signal
-from yodel.filter import Biquad
 
-input = signal.unit_impulse(48000, idx='mid')
-output = np.zeros(48000)
-bq = Biquad()
-bq.low_shelf(48000, 20, 0.707, 10)
-bq.process(input, output)
+fs = 48000
 
-nperseg = min(1 << (48000 - 1).bit_length(), output.shape[-1])
-f, Pxx_spec = signal.welch(output, 48000, nperseg=nperseg, scaling='spectrum', detrend=False)
-Pxx_spec = np.sqrt(Pxx_spec)
 
-mp.figure()
-mp.plot(f, 20.0 * np.log10(Pxx_spec))
-mp.xscale('log')
-mp.xlim(5, 200)
-mp.show()
+
+# Frequency response
+w, h = signal.freqz(b=bq.b, a=bq.a, worN=65536)
+# Generate frequency axis
+w = w * fs / (2 * np.pi)
+# Plot
+plt.semilogx(w, 20 * np.log10(np.abs(h)), 'b')
+plt.ylabel('Amplitude', color='b')
+plt.xlabel('Frequency')
+plt.show()
