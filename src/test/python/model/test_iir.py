ba87bca400433c4f592f704aa49c1aa8b35b507c
3ll3d00d <mattkhan@gmail.com>
2018-08-31 20:24:00 +0100

    added test for serialising a completefilter

--- src/test/python/model/test_iir.py
+++ src/test/python/model/test_iir.py
@@ -1,7 +1,8 @@
 import json
 
 from model.iir import ComplexLowPass, FilterType, from_json, ComplexHighPass, Passthrough, PeakingEQ, LowShelf, \
-    HighShelf, FirstOrder_LowPass, FirstOrder_HighPass, SecondOrder_LowPass, SecondOrder_HighPass, AllPass
+    HighShelf, FirstOrder_LowPass, FirstOrder_HighPass, SecondOrder_LowPass, SecondOrder_HighPass, AllPass, \
+    CompleteFilter
 
 
 def test_codec_Passthrough():
@@ -178,3 +179,38 @@ def test_codec_ComplexHighPass():
     assert filter.order == decoded.order
     assert filter.freq == decoded.freq
     assert decoded.getTransferFunction() is not None
+
+
+def test_codec_CompleteFilter():
+    filters = [PeakingEQ(1000, 50, 3.2, -5), LowShelf(1000, 25, 1, 3.2, count=3),
+               ComplexHighPass(FilterType.BUTTERWORTH, 6, 1000, 12)]
+    filter = CompleteFilter(filters=filters, description='Hello from me')
+    output = json.dumps(filter.to_json())
+    assert output == '{"_type": "CompleteFilter", "description": "Hello from me", "filters": [' \
+                     '{"_type": "PeakingEQ", "fs": 1000, "fc": 50, "q": 3.2, "gain": -5}, ' \
+                     '{"_type": "LowShelf", "fs": 1000, "fc": 25, "q": 1, "gain": 3.2, "count": 3}, ' \
+                     '{"_type": "ComplexHighPass", "filter_type": "BW", "order": 6, "fs": 1000, "fc": 12}' \
+                     ']}'
+    decoded = from_json(json.loads(output))
+    assert decoded is not None
+    assert isinstance(decoded, CompleteFilter)
+    assert decoded.description == 'Hello from me'
+    assert decoded.filters is not None
+    assert len(decoded.filters) == len(filter.filters)
+    assert decoded.getTransferFunction() is not None
+    assert isinstance(decoded.filters[0], filter.filters[0].__class__)
+    assert filter.filters[0].fs == decoded.filters[0].fs
+    assert filter.filters[0].q == decoded.filters[0].q
+    assert filter.filters[0].gain == decoded.filters[0].gain
+    assert filter.filters[0].freq == decoded.filters[0].freq
+    assert isinstance(decoded.filters[1], filter.filters[1].__class__)
+    assert filter.filters[1].fs == decoded.filters[1].fs
+    assert filter.filters[1].q == decoded.filters[1].q
+    assert filter.filters[1].gain == decoded.filters[1].gain
+    assert filter.filters[1].freq == decoded.filters[1].freq
+    assert filter.filters[1].count == decoded.filters[1].count
+    assert isinstance(decoded.filters[2], filter.filters[2].__class__)
+    assert filter.filters[2].fs == decoded.filters[2].fs
+    assert filter.filters[2].type == decoded.filters[2].type
+    assert filter.filters[2].order == decoded.filters[2].order
+    assert filter.filters[2].freq == decoded.filters[2].freq
