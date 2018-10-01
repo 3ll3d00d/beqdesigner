c37d44a909845185cdf916a026859d26b9bca9e7
3ll3d00d <mattkhan@gmail.com>
2018-10-01 08:48:42 +0100

    tweak script

--- release.bat
+++ release.bat
@@ -9,9 +9,9 @@ git commit -m"release: %VERSION%"
 git tag -am %VERSION% %VERSION%
 
 echo "Creating exe"
-pyinstaller --clean --log-level=ERROR -F for_exe.spec
+pyinstaller -y --clean --log-level=ERROR -F for_exe.spec
 echo "Creating installer content"
-pyinstaller --clean --log-level=WARN -D for_nsis.spec
+pyinstaller -y --clean --log-level=WARN -D for_nsis.spec
 echo "Creating installer"
 "C:\Program Files (x86)\NSIS\makensis" src\main\nsis\Installer.nsi
 
