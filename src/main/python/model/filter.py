3b7ea9b262a54c048e360cb15212c793d5bee719
3ll3d00d <mattkhan@gmail.com>
2019-03-21 22:10:21 +0000

    don't normalise_x in the filter dialog to avoid CPU hammering #224

--- src/main/python/model/filter.py
+++ src/main/python/model/filter.py
@@ -261,7 +261,7 @@ class FilterDialog(QDialog, Ui_editFilterDialog):
         self.__combined_preview = signal.filter
         # init the chart
         self.__magnitudeModel = MagnitudeModel('preview', self.previewChart, preferences, self, 'Filter',
-                                               db_range_calc=dBRangeCalculator(30), fill_curves=True)
+                                               db_range_calc=dBRangeCalculator(30), fill_curves=True, normalise_x=False)
         # load the selector and populate the fields
         self.__refresh_selector()
 
