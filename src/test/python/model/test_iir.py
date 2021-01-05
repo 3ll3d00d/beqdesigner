a432ad2f589ee5ebe2644fb17188d1853edf1682
3ll3d00d <mattkhan@gmail.com>
2021-01-05 22:25:50 +0000

    fix bad test comparison

--- src/test/python/model/test_iir.py
+++ src/test/python/model/test_iir.py
@@ -8,8 +8,8 @@ def test_lt():
     a0, a1, a2 = lt.a
     b0, b1, b2 = lt.b
     assert a0 == approx(1.0)
-    assert a1 == approx(1.993519840635900, rel=1e-12)
-    assert a2 == approx(-0.993540762888275, rel=1e-12)
+    assert a1 == approx(-1.993519840635900, rel=1e-12)
+    assert a2 == approx(0.993540762888275, rel=1e-12)
     assert b0 == approx(1.000037740241020, rel=1e-12)
     assert b1 == approx(-1.993508952524960, rel=1e-12)
     assert b2 == approx(0.993513910758182, rel=1e-12)
