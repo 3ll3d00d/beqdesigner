e6d342a71f6fbb391eb60a33f22a7ada163d5922
3ll3d00d <mattkhan@gmail.com>
2026-03-07 07:45:35 +0000

    another upgrade

--- src/main/python/ui/convert.sh
+++ src/main/python/ui/convert.sh
@@ -2,5 +2,5 @@
 for ui in $(ls *.ui)
 do
     echo "Compiling ${ui}"
-    /home/matt/.cache/pypoetry/virtualenvs/beqdesigner-IpT5f2Ps-py3.12/bin/pyuic6 "${ui}" -o "${ui%.ui}.py"
+    /home/matt/.cache/pypoetry/virtualenvs/beqdesigner-IpT5f2Ps-py3.13/bin/pyuic6 "${ui}" -o "${ui%.ui}.py"
 done
